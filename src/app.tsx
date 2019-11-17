34ab88fa13267ae82493b2b3b48daee5926227ee
Johan Li <johan@johanli.com>
2019-11-17 20:28:31 +0100

    use StaticRouter when rendering on the server

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { hot } from 'react-hot-loader/root';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Main from './components/Main';
 import './app.scss';
 
 const App = hot(Main);
 
-render(<App />, document.getElementById('root'));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
+);
