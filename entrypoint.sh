dde112c27d295d0a6797a870a05949810937d75c
Johan Li <johan@johanli.com>
2019-11-24 22:02:18 +0100

    use BuildKit to forward ssh when deploying

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp /tmp/.ssh/id_rsa /id_rsa
-chmod 0600 /id_rsa
-git clone --depth 1 https://github.com/JohanLi/johan.li.git
-cd /tmp/johan.li
-npm ci
-npm run build
-cd /tmp/johan.li/.ansible && ansible-playbook -i production --private-key /id_rsa site.yml
