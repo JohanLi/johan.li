aed46dacff07458ffff755f8e42b0d2f5ff35d1b
Johan Li <hi@johan.li>
2020-03-08 17:54:05 +0100

    update text for cargo cult article

--- src/articles/cargo-culting-in-software/Index.tsx
+++ src/articles/cargo-culting-in-software/Index.tsx
@@ -2,8 +2,7 @@ import React, { ReactElement } from 'react';
 
 import Image from '../../components/Image';
 
-import vanuatu from './vanuatu.jpg';
-import vanuatuLarge from './vanuatu-large.jpg';
+import vanuatuJohnFrumDay from './vanuatu-john-frum-day.jpg';
 import microservices from './microservices.png';
 import agile from './agile.png';
 import agileLarge from './agile-large.png';
@@ -36,10 +35,9 @@ const Index = (): ReactElement => (
       and equipment, falling from the skies?
     </p>
     <Image
-      src={vanuatu}
+      src={vanuatuJohnFrumDay}
       srcWidth={800}
-      srcHeight={531}
-      zoomSrc={vanuatuLarge}
+      srcHeight={600}
       caption="Vanuatu, an island country in the Pacific Ocean, has a cargo cult that survives to this day."
     />
     <p>
@@ -50,17 +48,16 @@ const Index = (): ReactElement => (
       waving tree branches.
     </p>
     <p>
-      They fell into the trap of confusing correlation with causation and formed
-      a cult — a cargo cult.
+      Having mistaken correlation for causation, they formed a cult — a cargo
+      cult.
     </p>
     <h2>In Software</h2>
     <p>
       In software, cargo culting can come in many different shapes and forms.
-      Sometimes, we copy-paste from Stack Overflow without understanding what
-      the snippet of code is doing. Occasionally, we run cryptic terminal
-      commands in an attempt to solve problems we don’t understand. We follow
-      conventions, patterns, and principles, yet are unable to explain why some
-      of them exist.
+      Sometimes, we copy-paste code snippets from Stack Overflow without
+      understanding how they work. Occasionally, we run terminal commands to
+      solve problems we don’t understand. We follow conventions, patterns, and
+      principles, yet are unable to explain why some of them exist.
     </p>
     <p>
       But beyond the individual lines of code, cargo culting can also affect
@@ -94,16 +91,15 @@ const Index = (): ReactElement => (
       on a feature that spans multiple services? Do we still benefit much from
       having independent components and teams? How do we orchestrate, monitor,
       and troubleshoot all our services? While great tools exist nowadays, they
-      are still tools we need to know about and know how to use well. In other
-      words, adopting microservices requires serious expertise.
+      are still tools we need to know about and know how to use well.
     </p>
     <p>
       At the same time, having a monolith doesn’t stop us from modularizing our
       code. We intend to adopt microservices because the monoliths we’ve worked
       on always turn out “messy.” If that’s the case, are we sure we’ll do a
-      good job splitting our monolith? If we struggle managing a single, albeit
-      larger, service, will things get better with microservices? Will we end up
-      with an even more complex monolith that we pretend are microservices?
+      good job splitting our monolith? If we struggle managing a single service,
+      will things get better with microservices? Will we end up with an even
+      more complex monolith that we pretend are microservices?
     </p>
     <p>
       Microservices are great and solve real problems. Otherwise, Amazon and
@@ -121,9 +117,9 @@ const Index = (): ReactElement => (
     </p>
     <p>
       These are questions we must answer. If we don’t, we might end up in a
-      scenario where four developers are working at a startup with a product in
-      a state of flux, maintaining ten poorly bounded microservices. Too busy
-      dealing with self-imposed problems, to be delivering value to end users.
+      scenario where two developers create ten microservices at a startup, for a
+      product that’s still in a state of flux. Too busy dealing with
+      self-imposed problems, to be delivering value to end users.
     </p>
     <h2>Agile</h2>
     <p>
@@ -152,53 +148,44 @@ const Index = (): ReactElement => (
       But having separate process people doesn’t lend itself well to software
       development. We’re knowledge workers. We’re well educated and well paid.
       Our work is highly contextual, seeing as software solves a wide array of
-      problems in all kinds of organizations, industries, and countries. Hence,
-      a pillar of Agile is letting actual workers decide how they work,
-      adjusting as they go along: “Individuals and Interactions over processes
-      and tools.”
+      problems in all kinds of organizations, industries, and countries.
+      Therefore, a pillar of Agile is letting actual workers decide how they
+      work, adjusting as they go along.
     </p>
     <p>
-      Another aspect of software development is the challenge in gathering
-      requirements and planning long-term. This ties into two other pillars of
-      Agile: “Responding to Change over following a plan” and “Working Software
-      over comprehensive documentation.” What’s not so apparent at first glance,
-      is that these two things also require a high degree of technical know-how,
-      similar to microservices. To respond to change and have working software,
-      we need developers who understand how to write modular code. Developers
-      who understand the importance of refactoring often. Developers who
-      understand continuous integration and how to write tests. And last but not
-      least, our organization as a whole needs to understand this.
+      As it also turns out, gathering requirements and planning long-term is
+      challenging in software development. Thus, another pillar of Agile is to
+      prefer responding to change over following a plan. But what’s not so
+      apparent, is that you need technical expertise to do this well. You need
+      developers who write modular code, create tests, set up continuous
+      integration, and refactor often. Last but not least, the rest of an
+      organization needs to understand this.
     </p>
     <p>
-      Yet, we enforce a one-size-fits-all process in our organization. We rename
-      project managers “scrum masters” and stakeholders “product owners.” We
-      rename our meetings “stand-ups,” buy planning poker decks, and start using
-      the word “sprints.” While this is going on, the command and control
-      mindset still permeates our organization. Hierarchies in communication
-      remain. We continue viewing our developers as assembly line workers. We
-      have neither plans to raise the technical skills of our developers, nor
-      plans to increase the software literacy of our entire organization.
+      Yet, contrary to the two pillars, we enforce a one-size-fits-all process.
+      We rename project managers “scrum masters” and stakeholders “product
+      owners.” We rename our meetings “stand-ups,” buy planning poker decks, and
+      start using the word “sprints.” While this is going on, the command and
+      control mindset still permeates our organization. Hierarchies in
+      communication remain. We continue viewing our developers as assembly line
+      workers. We have neither plans to raise the technical skills of our
+      developers, nor plans to increase the software literacy of our entire
+      organization.
     </p>
     <p>
       To top things off, we boast that we are now Agile. At a certain point, we
       have to stop and ask ourselves: are we that much different from the cargo
       culting natives? When we hire Agile coaches for the sole purpose of
-      telling us how to work, how far have we come from the charismatic
-      individuals maintaining cargo cults?
+      telling us how to work, how far have we come from the charismatic leaders
+      maintaining cargo cults?
     </p>
     <p>
-      Agile exists for a reason. But becoming Agile is hard. We can’t just mimic
-      superficial behaviors of other successful organizations and think we’ll
-      gain the same benefits. First off, it’s hard to tell how much Agile
-      contributed to their success. Secondly, and most importantly, we’re not
-      the same organization.
-    </p>
-    <p>
-      In the end, Agile is a set of values and principles. To best apply them,
-      we have to understand what type of organization we are, what industry we
-      work in, and what products and customers we have. Last but not least, we
-      mustn’t underestimate the value of actually talking to our people —
-      including our “average workers.”
+      In the end, Agile is a set of values and principles. To become Agile, we
+      can’t just mimic superficial behaviors of successful organizations,
+      thinking we’ll gain the same benefits. We’re not the same organization in
+      terms of industry, workforce, product, and customers. And most
+      importantly, we mustn’t underestimate the value of actually talking to our
+      people — including our “average workers” — as part of the process.
     </p>
     <h2>Thoughts</h2>
     <p>
@@ -206,12 +193,12 @@ const Index = (): ReactElement => (
     </p>
     <p>
       On realizing this, I strive to understand why I do and decide certain
-      things as a developer. I also strive to be better at explaining my
-      reasoning. By doing this, I show others what I’ve taken into
+      things as a developer. I also strive to be better at articulating my
+      thought process. By doing this, I show others what I’ve taken into
       consideration, giving them a chance to point out what I’ve missed. All the
-      companies we work for have cult-like characteristics to some degree.
-      Hence, every time I get a new colleague, I make it a habit to tell them
-      not to be afraid of questioning things.
+      companies we work for have cult-like characteristics to some degree. Every
+      time I get a new colleague, I make it a habit to tell them not to be
+      afraid of questioning things.
     </p>
     <p>
       On the flip side, cargo culting is not always bad. Life is complicated,
