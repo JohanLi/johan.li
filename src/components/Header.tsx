6e7a3493f064901bf8131a2e4481eab21dd351e0
Johan Li <johan@johanli.com>
2020-03-15 16:26:47 +0100

    use a minimalistic header instead. Visual tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,64 +1,22 @@
-import React, { ReactElement, useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import classNames from 'classnames';
+import React, { ReactElement } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 
 import styles from './header.scss';
 
 const Header = (): ReactElement => {
-  const [active, setActive] = useState(false);
-  const [transition, setTransition] = useState(false);
+  const { pathname } = useLocation();
 
-  const toggle = (): void => {
-    setActive(!active);
-    setTransition(true);
-  };
+  let title = <>Johan Li</>;
 
-  const navigate = (): void => {
-    setActive(false);
-    setTransition(false);
-  };
-
-  const header = classNames({
-    [styles.header]: true,
-    [styles.active]: active,
-    [styles.transition]: transition,
-  });
+  if (pathname === '/') {
+    title = <h1>{title}</h1>;
+  }
 
   return (
-    <header className={header}>
-      <div className={styles.navbar}>
-        <button className={styles.hamburger} onClick={toggle}>
-          <div className={styles.top} />
-          <div className={styles.mid} />
-          <div className={styles.bottom} />
-        </button>
-        <div className={styles.logo}>
-          <Link to="/">Johan.Li</Link>
-        </div>
-      </div>
-      <nav className={styles.menu}>
-        <ul className={styles.menuLinks}>
-          <li className={styles.menuLink}>
-            <NavLink
-              exact
-              to="/"
-              activeClassName={styles.menuLinkActive}
-              onClick={navigate}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className={styles.menuLink}>
-            <NavLink
-              to="/about"
-              activeClassName={styles.menuLinkActive}
-              onClick={navigate}
-            >
-              About
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header>
+      <Link to="/" className={styles.logo}>
+        {title}
+      </Link>
     </header>
   );
 };
