2f1565f56f560baae379edb77026e25b54d3c422
Johan Li <hi@johan.li>
2024-12-25 21:36:54 +0100

    new article

---
+++ app/(blog)/two-years-attending-a-writing-meetup/article.tsx
@@ -0,0 +1,122 @@
+import { H2 } from '../components/article/Common'
+import { P } from '../components/article/Common'
+import writingMeetup from './writing-meetup.jpg'
+
+const headings = ['How it began', 'Making friends', 'Challenges', 'The reward']
+
+const body = (
+  <>
+    <P>
+      I’ve formed a new habit lately: I attend a weekly in-person meetup. A
+      countdown begins, and we write in silence. Some might be writing a novel.
+      Others might be journaling. When the time’s up, we optionally start
+      socializing.
+    </P>
+    <P>
+      Having been a regular attendee, I eventually became one of the organizers.
+      It’s been an enriching experience—I’ve met wonderful people and, most
+      importantly, learned more about myself.
+    </P>
+    <H2>How it began</H2>
+    <P>
+      From 2021 onwards, I started working 100% remotely. As someone who never
+      had a social circle outside of work, I began missing the water cooler
+      moments as the months passed.
+    </P>
+    <P>
+      To fill that void, I attended various in-person events in 2023 to continue
+      pursuing solo hobbies but in the presence of others. Writing was one of
+      them, and I soon discovered and became a regular at the meetup I’d come to
+      organize.
+    </P>
+    <H2>Making friends</H2>
+    <P>
+      Decades ago, sociologists found that close friendships best form through
+      “proximity; repeated, unplanned interactions; and a setting that
+      encourages people to let their guard down and confide in each other.”
+    </P>
+    <P>
+      Unsurprisingly, most people attend the writing meetup to make friends,
+      myself included. But making friends when you’ve graduated from school is
+      like playing a game on hard mode. Natural meeting places have diminished
+      in this digital age. The fact we’re in Sweden cranks up the difficulty
+      even further—around strangers, we’re reserved and can come off as cold. We
+      also keep our social circles separate.
+    </P>
+    <P>
+      But here’s where the meetup has its place: we meet in person weekly. While
+      we indeed plan the meetups, new people show up every time. We work on
+      different things, and there are no set topics for what you discuss
+      afterward. If you attend, we don’t set expectations. It’s not an audition.
+      You don’t need to socialize or be a good writer. Heck, you don’t have to
+      write as long as you can stay silent until the countdown ends.
+    </P>
+    <H2>Challenges</H2>
+    <P>
+      Organizing meetups hasn’t always been easy, largely because of what I’ve
+      now realized. I’m a so-called high empath.
+    </P>
+    <P>
+      In group settings, that means I’m keenly aware of the energies and
+      emotions of each person and the room as a whole. If someone starts getting
+      bored or uncomfortable, I quickly pick up on that. I experience discomfort
+      if someone’s left out, whether because no one’s talking to them or the
+      current conversation excludes them.
+    </P>
+    <P>
+      I also feel strongly responsible for ensuring everyone has a good time
+      whenever I organize an outing. That’s why I’ve rarely done it, and on the
+      occasions I do, I’m selective about who I include.
+    </P>
+    <P>
+      As you can imagine, organizing a meetup, particularly an open one, is
+      exhausting. New people show up, often anxious. How can I calm them? What
+      are their expectations? They don’t seem to be having fun. Can I help them?
+    </P>
+    <P>
+      Since it’s open, all kinds of people show up. Not everyone has the best
+      social skills, and far from everyone are good conversationalists. You’ve
+      also got the oddballs and those who flat-out make others uncomfortable.
+      How will they affect the energies and emotions around them?
+    </P>
+    <P>
+      But as I’ve organized more, I’m learning to think more rationally. People
+      can’t set too high expectations. It’s free to attend, after all. They can
+      leave whenever. And anyone who wasn’t born yesterday knows that if you
+      meet random people, you won’t immediately click with many of them. I just
+      make sure to welcome attendees and set the correct expectations.
+    </P>
+    <H2>The reward</H2>
+    <P>
+      Overall, the meetup has been a great place to practice interpersonal
+      skills. I’ve gained a good understanding of the types of people I get
+      along with. With many of the attendees being expats, it feels like a
+      journey back in time to my elementary school days—an international setting
+      filled with classmates from dozens of countries and constant switching
+      between languages.
+    </P>
+    <P>
+      It also turns out that the sociologists were right. Two years later, I’ve
+      made some wonderful friends. We’ve bantered late into the evenings. We’ve
+      gone on boat trips to the Stockholm archipelago. I’ve learned more about
+      their respective cultures and received great book recommendations. Some
+      even came to my place for karaoke—a first but far from likely the last.
+    </P>
+    <P>
+      Another first is that I don’t recall saying I feel proud of myself. A
+      decade ago, I was a socially awkward person. But through years of personal
+      development, I can see that I’ve significantly improved. I no longer feel
+      like a fish out of water in social situations.
+    </P>
+  </>
+)
+
+export const article = {
+  thumbnail: writingMeetup,
+  title: 'Two years attending a writing meetup',
+  teaser: 'On making friends as an adult and personal development',
+  published: 1735150019,
+  readingTime: 3,
+  headings,
+  body,
+}
